cf0daaca7c5db39bc7db2b1fd78ed49ce8cf57e3
lucasnahas <lucasahas@gmail.com>
2021-08-04 14:37:21 -0300

    fix do req2

--- src/services/localStorage.jsx
+++ src/services/localStorage.jsx
@@ -1,34 +1,14 @@
-const StorageName = 'Trivia App';
-
 export const localStorageInit = () => {
-  const content = JSON.parse(localStorage.getItem(StorageName));
-  if (!content) {
-    const newContent = {
-      token: false,
-      player: {
-        name: false,
-        assertions: false,
-        score: false,
-        gravatarEmail: false,
-      },
-      ranking: [],
-    };
-    const save = JSON.stringify(newContent);
-    localStorage.setItem(StorageName, save);
-  }
   console.log('LUCAS: LOCAL STORAGE INITIATED');
 };
 
 export const loadTokenFromStorage = () => {
-  const content = JSON.parse(localStorage.getItem(StorageName));
-  const { token } = content;
+  const token = JSON.parse(localStorage.getItem('token'));
   if (!token) { return 'no token in storage'; }
   return token;
 };
 
 export const saveTokenToStorage = (token) => {
-  const content = JSON.parse(localStorage.getItem(StorageName));
-  const newContent = { ...content, token };
-  const save = JSON.stringify(newContent);
-  localStorage.setItem(StorageName, save);
+  const save = JSON.stringify(token);
+  localStorage.setItem('token', save);
 };
