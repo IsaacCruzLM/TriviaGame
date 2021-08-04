e055284494549bb853def5db1b12708767b33df0
Lucas Chamma Nahas <66845785+LucasChammaNahas@users.noreply.github.com>
2021-08-04 18:49:30 -0300

    Update index.js

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,18 +1,18 @@
 export const ADD_USER = 'ADD_USER';
-export const SAVE_PLAYER_INFO = 'SAVE_PLAYER_INFO';
+// export const SAVE_PLAYER_INFO = 'SAVE_PLAYER_INFO';
 
-export const addUser = (user) => (
-  {
-    type: ADD_USER,
-    user,
-  }
-);
+// export const addUser = (user) => (
+//   {
+//     type: ADD_USER,
+//     user,
+//   }
+// );
 
 
-export const savePlayerInfo = (player) => (
-  {
-    type: SAVE_PLAYER_INFO,
-    player,
+// export const savePlayerInfo = (player) => (
+//   {
+//     type: SAVE_PLAYER_INFO,
+//     player,
 
 // export const DECREASE_TIME = 'DECREASE_TIME';
 
@@ -22,18 +22,18 @@ export const savePlayerInfo = (player) => (
 //   }
 // );
 
-export const STOP_TIME = 'STOP_TIME';
+// export const STOP_TIME = 'STOP_TIME';
 
-export const stopTime = () => (
-  {
-    type: STOP_TIME,
-  }
-);
+// export const stopTime = () => (
+//   {
+//     type: STOP_TIME,
+//   }
+// );
 
-export const RESET_TIME = 'RESET_TIME';
+// export const RESET_TIME = 'RESET_TIME';
 
-export const resetTime = () => (
-  {
-    type: RESET_TIME,
-  }
-);
+// export const resetTime = () => (
+//   {
+//     type: RESET_TIME,
+//   }
+// );
