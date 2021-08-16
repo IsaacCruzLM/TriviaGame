d413c8205aa26a39d2a467383973ba87379f16ce
Isaac Cruz <isaac.clm1@gmail.com>
2021-08-16 20:23:56 -0300

    Updates

---
+++ precache-manifest.744846b422e5123db65d47d5414d8ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9216471f5a8a6a5bbff9b4f0831fb227",
+    "url": "/TriviaGame/index.html"
+  },
+  {
+    "revision": "171c1f5bbf524785c2f6",
+    "url": "/TriviaGame/static/css/main.9ec39fe1.chunk.css"
+  },
+  {
+    "revision": "3738cafac4af21180064",
+    "url": "/TriviaGame/static/js/2.177664f5.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/TriviaGame/static/js/2.177664f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "171c1f5bbf524785c2f6",
+    "url": "/TriviaGame/static/js/main.b2209aab.chunk.js"
+  },
+  {
+    "revision": "46231e653e79ce35fa1b",
+    "url": "/TriviaGame/static/js/runtime-main.4c9a0e6c.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/TriviaGame/static/media/trivia.466d153e.png"
+  }
+]);
