d051aa12edffba29d17309451d10ebf88f62db09
Isaac Cruz <isaac.clm1@gmail.com>
2021-08-16 20:19:52 -0300

    Pronto para Deploy

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
+      <BrowserRouter basename={ process.env.PUBLIC_URL }>
         <App />
       </BrowserRouter>
     </Provider>
